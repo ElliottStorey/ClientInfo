263b2636e6cb01b4a8a8e34d6b6b19bdff949813
Elliott Storey <70775866+ElliottStorey@users.noreply.github.com>
2022-05-15 17:10:32 -0500

    Update GeolocationAPIPlus.js

--- GeolocationAPIPlus.js
+++ GeolocationAPIPlus.js
@@ -1,1 +1,23 @@
+async function getCurrentPosition(success, error, options) {
+  if('geolocation' in navigator) {
+    /* geolocation is available */
+  } else {
+    /* geolocation IS NOT available */
+  }
+}
 
+async function watchPosition(success, error, options) {
+  if('geolocation' in navigator) {
+    /* geolocation is available */
+  } else {
+    /* geolocation IS NOT available */
+  }
+}
+
+async function clearWatch(success, error, options) {
+  if('geolocation' in navigator) {
+    /* geolocation is available */
+  } else {
+    /* geolocation IS NOT available */
+  }
+}
