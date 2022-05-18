ff8fd339c969cfc97b7f2da42dfe5f82e0aca3f0
Elliott Storey <70775866+ElliottStorey@users.noreply.github.com>
2022-05-17 19:37:11 -0500

    Update geo+.js

--- geo+.js
+++ geo+.js
@@ -1,3 +1,26 @@
+const script = document.createElement("script");
+script.setAttribute(
+  "src",
+  "http://api.ipify.org?format=jsonp&callback=getIP",
+);
+
+async function getIP(json) {
+  return(json.ip);
+}
+
+async function success+(GeolocationPosition) {
+  return(GeolocationPosition);
+}
+
+async function getCurrentPosition+(error) {
+  console.log(error);
+  let ip = getIP();
+  let url = `https://ipinfo.io/${ip}/json`;
+  let data = await fetch(url);
+  data = await data.json();
+  console.log(data);
+}
+
 async function getCurrentPosition(success, error, options) {
   if('geolocation' in navigator) {
     /* geolocation is available */
