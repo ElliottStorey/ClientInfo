b085e668fb53b240017fcd18e1344d1ac0cd4469
Elliott Storey <70775866+ElliottStorey@users.noreply.github.com>
2022-05-19 09:09:00 -0500

    Update geo+.js

--- geo+.js
+++ geo+.js
@@ -8,28 +8,30 @@ async function getIP(json) {
   return(json.ip);
 }
 
-async function success+(GeolocationPosition) {
-  return(GeolocationPosition);
-}
-
-async function getCurrentPosition(GeolocationPositionError) {
-  console.log(GeolocationPositionError);
-  let ip = getIP();
-  let url = `https://ipinfo.io/${ip}/json`;
-  let data = await fetch(url);
-  data = await data.json();
-  console.log(data);
+async function getCurrentPosition+(success, error, options) {
+  try {
+    let ip = getIP();
+    let url = `https://ipinfo.io/${ip}/json`;
+    let data = await fetch(url);
+    data = await data.json();
+    console.log(data);
+    let GeolocationPosition = {};
+    await success(GeolocationPosition);
+  } catch (GeolocationPositionError) {
+    await error(GeolocationPositionError);
+  }
 }
 
 async function getCurrentPosition(success, error, options) {
   try {
     if("geolocation" in navigator) {
-      
+      await navigator.geolocation.getCurrentPosition(success, error, options);
     } else {
-      
+      await getCurrentPosition+(success, error, options);
     }
-  } catch (err) {
-    error(err);
+  } catch (GeolocationPositionError) {
+    console.log(GeolocationPositionError);
+    getCurrentPosition+(success, error, options);
   }
 }
 
