e25b1a31e1586bf951bdd1d0c9044e6732f4bf66
Elliott Storey <70775866+ElliottStorey@users.noreply.github.com>
2022-05-20 11:33:06 -0500

    Update geo+.js

--- geo+.js
+++ geo+.js
@@ -4,39 +4,63 @@ fetch("https://api.ipify.org").then(response => {
   });
 });
 
-async function getCurrentPosition+(success, error, options) {
-  try {
-    let ip = window.ip;
-    let url = `https://ipinfo.io/${ip}/json`;
-    let data = await fetch(url);
-    data = await data.json();
-    console.log(data);
-    let GeolocationPosition = {};
-    await success(GeolocationPosition);
-  } catch (GeolocationPositionError) {
-    await error(GeolocationPositionError);
-  }
+async function data() {
+  let ip = window.ip;
+  let url = `https://ipinfo.io/${ip}/json`;
+  let data = await fetch(url);
+  data = await data.json();
+  return data;
 }
 
-async function getCurrentPosition(success, error, options) {
-  try {
-    if("geolocation" in navigator) {
-      await navigator.geolocation.getCurrentPosition(success, error, options);
-    } else {
-      await getCurrentPosition+(success, error, options);
+async function getCurrentPositionPlus() {
+  let coords = await data();
+  let GeolocationCoordinates = {
+    accuracy: null,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: coords.loc.split(",")[0],
+    longitude: coords.loc.split(",")[1],
+    speed: null
+  }
+  let GeolocationPosition = {
+    coords: GeolocationCoordinates,
+    timestamp: + new Date()
+  };
+  return GeolocationPosition;
+}
+
+async function getCurrentPosition(finalSuccess, finalError, options) {
+  async function success(GeolocationPosition) {
+    try {
+      let GeolocationPositionPlus = Object.assign(GeolocationPosition, await data());
+      await finalSuccess(GeolocationPositionPlus);
+    } catch (error) {
+      await finalError(error);
     }
-  } catch (GeolocationPositionError) {
-    console.log(GeolocationPositionError);
-    getCurrentPosition+(success, error, options);
+  }
+  async function error(error) {
+    try {
+      console.log(error);
+      let GeolocationPositionPlus = Object.assign(await getCurrentPositionPlus(), await data());
+      await finalSuccess(GeolocationPositionPlus);
+    } catch (error) {
+      await finalError(error);
+    }
+  }
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  } else {
+    await error();
   }
 }
 
 async function watchPosition(success, error, options) {
   try {
-    if("geolocation" in navigator) {
-      
+    if ("geolocation" in navigator) {
+
     } else {
-      
+
     }
   } catch (err) {
     error(err);
@@ -45,10 +69,10 @@ async function watchPosition(success, error, options) {
 
 async function clearWatch(success, error, options) {
   try {
-    if("geolocation" in navigator) {
-      
+    if ("geolocation" in navigator) {
+
     } else {
-      
+
     }
   } catch (err) {
     error(err);
